2285a1de122fb4478bcc48d85159cdb7320f1ba4
Patrick J Brown <PatrickJBrown@outlook.com>
2025-05-21 22:43:14 -0400

    Adding problems 2 and 3

---
+++ Problems/Problem3.cs
@@ -0,0 +1,44 @@
+﻿namespace ProjectEuler.Problems
+{
+    public class Problem3 : Problem
+    {
+        public Problem3()
+        {
+            Id = 3;
+            Name = "Largest Prime Factor";
+            Description =
+                "    The prime factors of 13195 are 5, 7, 13, and 29.\n" +
+                "    What is the largest prime factor of the number 600851475143?";
+        }
+
+        public override string GetSolution()
+        {
+            return GetLargestPrimeFactor(600851475143L).ToString();
+        }
+
+        private long GetLargestPrimeFactor(long n)
+        {
+            n = Math.Abs(n);
+
+            while (n % 2 == 0)
+                n /= 2;
+
+            long divisor = 3;
+            long sqrtN = (long)Math.Floor(Math.Sqrt(n));
+            while (divisor <= sqrtN)
+            {
+                if (n % divisor == 0)
+                {
+                    n /= divisor;
+                    sqrtN = (long)Math.Floor(Math.Sqrt(n));
+                }
+                else
+                {
+                    divisor += 2;
+                }
+            }
+
+            return n;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 A collection of coding solutions to the algorithm / mathematics problems from [ProjectEuler.net](https://projecteuler.net/about)
 
 ## Build/Run Instructions
-This is a command-line application with no specail steps needed to build. Simply start the Visual Studio debugger.
+This is a command-line application with no special steps needed to build. Simply start the Visual Studio debugger, and follow the instructions in the command prompt.
+
+Though in fairness, the answers themselves aren't particularly interesting. It's the code for the solutions that matters. Please feel free to examine the entries in `\Problems`. Each file contains a description of the task/puzzle along with the code that solves it.
 
 ## Skills / Concepts / Technologies
 This project showcases the following skills, concepts, and technologies:
