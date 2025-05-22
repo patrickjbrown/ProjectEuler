104328d870a48e3effbfc6ca622cecbfb05ea654
Patrick J Brown <PatrickJBrown@outlook.com>
2025-05-22 17:05:56 -0400

    Adding problems 6, 7, and 8.

---
+++ Problems/Problem6.cs
@@ -0,0 +1,43 @@
+﻿namespace ProjectEuler.Problems
+{
+    public class Problem6 : Problem
+    {
+        public Problem6()
+        {
+            Id = 6;
+            Name = "Sum Square Difference";
+            Description =
+                "    The sum of the squares of the first ten natural numbers is,\n" +
+                "      (1^2) + (2^2) + ... + (10^2) = 385\n" +
+                "    The square of the sum of the first ten natural numbers is,\n" +
+                "      (1 + 2 + ... + 10) = 3025\n" +
+                "    Hence the difference between the sum of the squares of the first ten natural\n" +
+                "    numbers and the square of the sum is: 3025 - 385 = 2640\n\n" +
+                "    Find the difference between the sum of the squares of the first one hundred\n" +
+                "    natural numbers and the square of the sum.";
+        }
+
+        /**
+         * NOTES:
+         * 
+         * (1) I'm sure there is some very fancy math that involves calculations on series that
+         *     would solve this problem faster, but my calculus days are long behind me.
+         */
+
+        public override string GetSolution()
+        {
+            return (SquareOfSum(100) - SumOfSquares(100)).ToString();
+        }
+
+        private long SumOfSquares(int n)
+        {
+            return Enumerable.Range(1, n).Sum(x => x * x);
+        }
+
+        private long SquareOfSum(int n)
+        {
+            long sum = Enumerable.Range(1, n).Sum();
+            return sum * sum;
+        }
+    }
+}
---
+++ Problems/Problem7.cs
@@ -0,0 +1,66 @@
+﻿namespace ProjectEuler.Problems
+{
+    public class Problem7 : Problem
+    {
+        public Problem7()
+        {
+            Id = 7;
+            Name = "10,001st Prime";
+            Description =
+                "    By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see\n" +
+                "    that the 6th prime is 13.\n\n" +
+                "    What is the 10,001st prime number?";
+        }
+
+        /**
+         * NOTES:
+         * 
+         * (1) We start with base knowledge of just two primes: 2 and 3. To find the next prime,
+         *     we increment the candidate by 2 (since all primes must be odd).
+         *     
+         * (2) If a number is non-prime, then it must have prime factors. So we check to see if
+         *     the candidate is divisible by any of our known primes. If not, it must be prime,
+         *     and can be added to the list.
+         *     
+         * (3) However, we do not need to check against ALL of our known primes. We only need to
+         *     evaluate those that are less than or equal to the square root of the candidate,
+         *     since if a number has a prime factor > sqrt(N), then the corresponding factor must
+         *     have been less than sqrt(N).
+         */
+
+        public override string GetSolution()
+        {
+            return GetNthPrimeNumber(10001).ToString();
+        }
+
+        private long GetNthPrimeNumber(int n)
+        {
+            List<long> primes = new List<long> { 2, 3 };
+            long candidate = 3;
+
+            while(primes.Count < n)
+            {
+                candidate += 2;
+                long sqrt = (long)Math.Floor(Math.Sqrt(candidate));
+
+                bool isPrime = true;
+                foreach (long prime in primes)
+                {
+                    if (prime > sqrt)
+                        break;
+
+                    if (candidate % prime == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                if (isPrime)
+                    primes.Add(candidate);
+            }
+
+            return primes.Last();
+        }
+    }
+}
